e4e9fcb891ca2400369e3172645a166ac3876491
Junda Yin <junda.yin1@gmail.com>
2025-08-25 15:02:35 -0500

    Finished building section & page renderer, and mostly done with CMS.

--- site/app/not-found.tsx
+++ site/app/not-found.tsx
@@ -1,6 +1,6 @@
 const defaultMessage = <p className="H4" style={{textAlign: 'center'}}>Sorry, we couldn&apos;t find the page you&apos;re looking for.</p>;
 export default async function Page404({ customMessage }: { customMessage?: React.ReactNode }) {
-  return <div className="SectionRoot" style={{height: '100vh'}}>
+  return <div className="SectionRoot" style={{height: '90vh'}}>
     <div className="SectionInner" style={{gap: '1rem'}}>
       <h1 className="Title">404</h1>
       <div className="BodyLarge">{customMessage || defaultMessage}</div>
