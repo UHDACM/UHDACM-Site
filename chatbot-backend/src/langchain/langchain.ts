19c7ca37a5e6672c53701bb7eaeea29a3c3c6b41
Vincent V <vince.dev.help@gmail.com>
2026-01-08 22:38:59 -0600

    fix: removed test func, console log and exported func

--- chatbot-backend/src/langchain/langchain.ts
+++ chatbot-backend/src/langchain/langchain.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai';
 
-async function handleQuestion(question: string) {
+export async function handleQuestion(question: string) {
   const model = new ChatGoogleGenerativeAI({
     model: 'gemini-2.5-flash-lite',
     apiKey: process.env.GOOGLE_API_KEY,
@@ -9,8 +9,6 @@ async function handleQuestion(question: string) {
   const response = await model.invoke(question);
 
   return typeof response.content === 'string'
-    ? response.content && console.log(response.content)
+    ? response.content
     : JSON.stringify(response.content);
 }
-
-handleQuestion('What comes first 6 or 7?');
